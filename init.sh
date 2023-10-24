5078732eafc73c35b645652d64ff3184485fa604
snuggs <rashaunstovall@gmail.com>
2023-10-24 04:44:45 -0400

    add init.sh

---
+++ init.sh
@@ -0,0 +1,40 @@
+pkg upgrade
+pkg update
+pkg install termux-api
+termux-setup-storage
+pkg install man
+pkg install git
+pkg install tmux
+pkg install wget
+pkg install golang
+pkg install youtubedr
+pkg install ffmpeg
+pkg install vlc
+vlc --version
+pkg install ruby
+pkg install nodejs-lts
+npm install -g npm
+pkg install htop
+pkg install jq
+pkg install python
+pip list
+pkg install python-numpy
+pkg install asciinema
+pkg install sqlite
+pkg install vim
+gem update --system
+gem help env
+ruby --version
+gem --version
+node --version
+npm --version
+python --version
+pip --version
+go version
+git --version
+vim --version
+ffmpeg -version
+tmux -V
+pkg install exa
+echo
+source ~/.bashrc
