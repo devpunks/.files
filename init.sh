bcc259b56c7eeba656fda5562a24f500183e0942
snuggs <rashaunstovall@gmail.com>
2024-03-16 21:39:55 +0000

    Refactor mail client

--- init.sh
+++ init.sh
@@ -52,7 +52,26 @@ pkg install ncurses
 pkg install cmatrix
 pkg install shellcheck
 
-# Security ----------------------------------------------------------------
+# }}}
+
+# {{{ Mail ----------------------------------------------------------------
+
+# MAIL CLIENTS - https://www.reddit.com/r/commandline/comments/p36vp0
+#   - https://meli-email.org/
+#   - https://github.com/neomutt/neomutt
+#   - https://github.com/lukesmithxyz/mutt-wizard
+#   - https://mailutils.org/manual/html_section/mail.html
+#   - https://bence.ferdinandy.com/2023/07/20/email-in-the-terminal-a-complete-guide-to-the-unix-way-of-email/
+#   - https://web.archive.org/web/20210126200507/https://chiraag.me/blog/2019/08/21/managing-multiple-email-accounts-with-mutt-and-fetchmail/
+
+pkg install mutt
+pkg install mailutils
+pkg install fetchmail # https://www.fetchmail.info
+
+# }}}
+
+# {{{ Security ------------------------------------------------------------
+
 pkg install whois
 pkg install gnupg
 pkg install strace
