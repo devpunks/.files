5074a3dd46db2c01d4f029fb7392552c74adfaa8
snuggs <rashaunstovall@gmail.com>
2024-04-01 19:37:43 +0000

    Remove rar

--- init.sh
+++ init.sh
@@ -139,7 +139,6 @@ pkg install rsync
 pkg install brotli
 pkg install zopfli
 pkg install logrotate
-# RAR is patented - https://github.com/termux/termux-packages/issues/2190#issuecomment-367917474
 
 # }}}
 
