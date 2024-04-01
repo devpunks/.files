29cf93cf08ee3ee9319f24c6590b4c6ec3dcf4e0
snuggs <rashaunstovall@gmail.com>
2024-04-01 19:54:26 +0000

    Refactor documentation on init

--- init.sh
+++ init.sh
@@ -64,7 +64,8 @@ pkg install shellcheck
 #   - https://github.com/lukesmithxyz/mutt-wizard
 #   - https://mailutils.org/manual/html_section/mail.html
 #   - https://bence.ferdinandy.com/2023/07/20/email-in-the-terminal-a-complete-guide-to-the-unix-way-of-email/
-#   - https://web.archive.org/web/20210126200507/https://chiraag.me/blog/2019/08/21/managing-multiple-email-accounts-with-mutt-and-fetchmail/
+#   - https://web.archive.org/web/20210126200507/
+#   - https://chiraag.me/blog/2019/08/21/managing-multiple-email-accounts-with-mutt-and-fetchmail/
 
 pkg install mutt
 pkg install mailutils
