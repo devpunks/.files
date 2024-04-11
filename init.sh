79e2b681fb049dee9447d66d0a166537e54a00a7
snuggs <rashaunstovall@gmail.com>
2024-04-11 03:59:17 +0000

    Remove www

--- init.sh
+++ init.sh
@@ -58,7 +58,7 @@ pkg install shellcheck # https://shellcheck.net
 
 # {{{ Mail ----------------------------------------------------------------
 
-# MAIL CLIENTS - https://www.reddit.com/r/commandline/comments/p36vp0
+# MAIL CLIENTS - https://reddit.com/r/commandline/comments/p36vp0
 #   - https://meli-email.org/
 #   - https://github.com/neomutt/neomutt
 #   - https://github.com/lukesmithxyz/mutt-wizard
