13e01a9b595daac1653cc13f2610fbb304c9cde6
snuggs <rashaunstovall@gmail.com>
2024-06-24 22:24:43 +0000

    Add uuid-utils to init.sh

--- init.sh
+++ init.sh
@@ -52,6 +52,7 @@ pkg install tree
 pkg install which
 pkg install ncurses
 pkg install cmatrix
+pkg install uuid-utils # uuidgen
 pkg install shellcheck # https://shellcheck.net
 # pkg install expect
 
