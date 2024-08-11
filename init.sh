db03c6f9dfa58fbb4a8382e6ef418dac1475b048
snuggs <rashaunstovall@gmail.com>
2024-08-11 04:57:26 +0000

    Add traceroute package

--- init.sh
+++ init.sh
@@ -121,6 +121,7 @@ echo TODO: Install yt-dlp https://github.com/yt-dlp/yt-dlp
 #   - https://github.com/termux/termux-packages/issues/12758
 pkg install iproute2 # ip addr instad of ifconfig -a
 pkg install net-tools # https://net-tools.sourceforge.io
+pkg install traceroute # https://traceroute.sourceforge.net/
 # NMAP
 #   - https://upguard.com/blog/how-to-use-nmap
 #   - https://redhat.com/sysadmin/use-cases-nmap
