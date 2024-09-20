30b8069d7b66711a064346dc6de3cb215aabcb83
snuggs <rashaunstovall@gmail.com>
2024-09-20 07:40:53 +0000

    Remove solidus

--- init.sh
+++ init.sh
@@ -73,7 +73,7 @@ pkg install weechat
 #  - https://github.com/lukesmithxyz/mutt-wizard
 #  - https://mailutils.org/manual/html_section/mail.html
 #  - https://bence.ferdinandy.com/2023/07/20/email-in-the-terminal-a-complete-guide-to-the-unix-way-of-email/
-#  - https://chiraag.me/blog/2019/08/21/managing-multiple-email-accounts-with-mutt-and-fetchmail/
+#  - https://chiraag.me/blog/2019/08/21/managing-multiple-email-accounts-with-mutt-and-fetchmail
 
 pkg install mutt
 pkg install mailutils
