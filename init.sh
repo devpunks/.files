359fa22876de7ccd3ccc2d4ed14a26563731d3b7
snuggs <rashaunstovall@gmail.com>
2024-10-10 00:04:00 +0000

    Remove www

--- init.sh
+++ init.sh
@@ -138,7 +138,7 @@ pkg install netcat-openbsd
 
 pkg install nginx
 pkg install apache2 # https://httpd.apache.org/docs
-pkg install lighttpd # https://www.lighttpd.net
+pkg install lighttpd # https://lighttpd.net
 pkg install tinyproxy # https://tinyproxy.github.io
 
 # }}}
