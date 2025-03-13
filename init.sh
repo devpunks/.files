92bd78b8625d04d4235218f184d8f25017aa08d6
snuggs <rashaunstovall@gmail.com>
2025-03-13 19:23:28 +0000

    Update strace documentation

--- init.sh
+++ init.sh
@@ -92,7 +92,7 @@ pkg install tsu # https://pypi.org/project/tsu
 
 pkg install whois # mkpasswd
 pkg install gnupg # https://gnupg.org
-pkg install strace
+pkg install strace # https://man7.org/linux/man-pages/man1/strace.1.html
 pkg install argon2 # https://en.m.wikipedia.org/wiki/Argon2
 pkg install openssl # https://openssl.org
 pkg install openssh # https://openssh.com/manual.html
