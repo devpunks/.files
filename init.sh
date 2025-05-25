dabd017f0aefed31fbe7a03589832b71bc371111
snuggs <rashaunstovall@gmail.com>
2025-05-25 16:37:24 +0000

    Update submodules

--- init.sh
+++ init.sh
@@ -235,6 +235,7 @@ echo Cloning submodules...
 # https://github.com/lambdalisue/vim-fern-bookmark
 # https://github.com/lambdalisue/vim-fern-git-status
 # https://github.com/vim-scripts/jellybeans.vim
+# https://github.com/lifepillar/vim-mucomplete
 # https://github.com/Asheq/close-buffers.vim
 # https://github.com/airblade/vim-gitgutter
 
