da3516cfae5b56a707c194c68a3c67618782ea36
snuggs <rashaunstovall@gmail.com>
2026-05-06 19:10:58 +0000

    Add openssl-tool

--- init.sh
+++ init.sh
@@ -125,7 +125,7 @@ pkg install gnupg # https://gnupg.org
 pkg install strace # https://man7.org/linux/man-pages/man1/strace.1.html
 pkg install argon2 # https://en.m.wikipedia.org/wiki/Argon2
 pkg install scrypt # https://tarsnap.com/scrypt.html
-pkg install openssl # https://openssl.org
+pkg install openssl openssl-tool # https://openssl.org
 pkg install openssh # https://openssh.com/manual.html
 pkg install sharutils # https://gnu.org/software/sharutils
 
