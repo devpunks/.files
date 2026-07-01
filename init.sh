b5439be881a0c02590e4c9a110605395a798d40f
snuggs <rashaunstovall@gmail.com>
2026-07-01 18:12:05 +0000

    Add jpegoptim package

--- init.sh
+++ init.sh
@@ -270,6 +270,7 @@ pkg install texinfo
 pkg install sox
 pkg install vlc
 pkg install optipng # https://optipng.sourceforge.net
+pkg install jpegoptim # https://man.archlinux.org/man/extra/jpegoptim/jpegoptim.1.en
 pkg install libvips --fix-missing # https://github.com/libvips/libvips
 pkg install ffmpeg
 pkg install graphviz
