8d4194cd5549a7b7deff950f7c1f177cad103fa2
snuggs <rashaunstovall@gmail.com>
2026-07-01 20:26:38 +0000

    Add documentation for pngquant

--- init.sh
+++ init.sh
@@ -269,7 +269,7 @@ pkg install texinfo
 # {{{ Multimedia ----------------------------------------------------------
 pkg install sox
 pkg install vlc
-pkg install pngquant
+pkg install pngquant # $$ https://pngquant.org
 pkg install optipng # TODO: (Deprecate in lieu of pngquant) - https://optipng.sourceforge.net
 pkg install jpegoptim # https://man.archlinux.org/man/extra/jpegoptim/jpegoptim.1.en
 pkg install libvips --fix-missing # https://github.com/libvips/libvips
