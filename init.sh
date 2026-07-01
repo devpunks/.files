dda77f608020ed4d3876acffb82abf6bf050c2cc
snuggs <rashaunstovall@gmail.com>
2026-07-01 20:43:26 +0000

    Update documentation

--- init.sh
+++ init.sh
@@ -272,6 +272,7 @@ pkg install vlc
 pkg install pngquant # $$ https://pngquant.org
 pkg install optipng # TODO: (Deprecate in lieu of pngquant) - https://optipng.sourceforge.net
 pkg install jpegoptim # https://man.archlinux.org/man/extra/jpegoptim/jpegoptim.1.en
+# --no-install-recommends disables installing ImageMagick
 pkg install libvips --no-install-recommends --fix-missing # https://github.com/libvips/libvips
 pkg install ffmpeg
 pkg install graphviz
